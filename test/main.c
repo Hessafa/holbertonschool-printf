1ba51b9f129313693628869e47d6cd6892d6961e
Hessafa <10685@holbertonstudents.com>
2025-03-23 20:20:33 +0000

    s

--- test/main.c
+++ test/main.c
@@ -1,60 +1,44 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string
- * Return: Number of characters printed (excluding null byte)
+ * main - Entry point
+ *
+ * Return: Always 0
  */
-int _printf(const char *format, ...)
+int main(void)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char *str;
-    char ch;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-    va_start(args, format);
-
-    while (format && format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')  // Handle character
-            {
-                ch = (char)va_arg(args, int);
-                write(1, &ch, 1);
-                count++;
-            }
-            else if (format[i] == 's')  // Handle string
-            {
-                str = va_arg(args, char*);
-                while (str && *str)
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-            }
-            else if (format[i] == '%')  // Handle percent sign
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else  // Handle invalid format
-            {
-                write(1, &format[i - 1], 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
