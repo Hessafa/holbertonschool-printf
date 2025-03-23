ddfc636cc48d5c5a7636c4b5b25ed873a45356e5
Hessafa <10685@holbertonstudents.com>
2025-03-23 19:48:27 +0000

    first

--- main.h
+++ main.h
@@ -1,35 +1,9 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
+/* ======= LIBRARIES ======= */
+#include <stdarg.h>  /*  For va_list, va_start, va_arg, va_end */
+#include <unistd.h>   /* For write function */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
-
-/**
- * struct printf - Struct printf
- *
- * @printf: The operator
- * @f: The function asso
- */
-typedef struct printf
-{
-	char *printf;
-	void (*f)(char *buff, va_list);
-} printf_t;
-
-
-int _putchar(char c);
-int _strlen(char *s);
 int _printf(const char *format, ...);
-void function_char(char *buf, va_list valist);
-void function_int(char *buff, va_list valist);
-void function_s(char *buff, va_list valist);
-void function_percent(char *buff, va_list valist);
-void function_binary(char *buff, va_list valist);
-void rev_string(char *buff, va_list valist);
-int check_case(char s);
-void (*get_case(const char *format, int i))(char *buff, va_list valist);
 
-#endif
+#endif /* MAIN_H */
