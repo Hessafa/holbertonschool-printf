7e4ba8735b62a515cdc81a252f7a4da6426b733e
Hessafa <10685@holbertonstudents.com>
2025-03-24 18:03:20 +0000

    Initial commit

--- main.h
+++ main.h
@@ -2,13 +2,11 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <unistd.h>
 
-/* Function Prototypes */
+int _putchar(char c);
 int _printf(const char *format, ...);
 int handle_char(va_list args);
 int handle_string(va_list args);
 int handle_percent(void);
-int _putchar(char c);
 
 #endif /* MAIN_H */
