b975b2d8ab7e340442023f54eeb284a0a8660a1d
Hessafa <10685@holbertonstudents.com>
2025-03-24 18:15:16 +0000

    first task

--- printf.c
+++ printf.c
@@ -1,93 +1,88 @@
 #include "main.h"
-
 /**
- * print_char - Prints a single character.
- * @args: The argument list containing the character.
- *
- * Return: Number of characters printed.
- */
+* print_char - Prints a single character.
+* @args: The argument list containing the character.
+*
+* Return: Number of characters printed.
+*/
 int print_char(va_list args)
 {
-	char c = va_arg(args, int);
+char c = va_arg(args, int);
 
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
-
 /**
- * print_string - Prints a string.
- * @args: The argument list containing the string.
- *
- * Return: Number of characters printed.
- */
+* print_string - Prints a string.
+* @args: The argument list containing the string.
+*
+* Return: Number of characters printed.
+*/
 int print_string(va_list args)
 {
-	char *str = va_arg(args, char *);
-	int count = 0;
+char *str = va_arg(args, char *);
+int count = 0;
 
-	if (!str)
-		str = "(null)";
+if (!str)
+str = "(null)";
 
-	while (*str)
-		count += write(1, str++, 1);
+while (*str)
+count += write(1, str++, 1);
 
-	return (count);
+return (count);
 }
 
 /**
- * print_percent - Prints a literal percent sign.
- *
- * Return: Number of characters printed.
- */
+* print_percent - Prints a literal percent sign.
+*
+* Return: Number of characters printed.
+*/
 int print_percent(void)
 {
-	return (write(1, "%", 1));
+return (write(1, "%", 1));
 }
-
 /**
- * _printf - Produces output according to a format.
- * @format: A character string containing directives.
- *
- * Return: Number of characters printed (excluding null byte),
- *         or -1 if an error occurs.
- */
+* _printf - Produces output according to a format.
+* @format: A character string containing directives.
+*
+* Return: Number of characters printed (excluding null byte),
+*         or -1 if an error occurs.
+*/
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+va_list args;
+int count = 0;
 
-	if (!format)
-		return (-1);
+if (!format)
+return (-1);
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (!*format)
-				return (-1);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (!*format)
+return (-1);
 
-			if (*format == 'c')
-				count += print_char(args);
-			else if (*format == 's')
-				count += print_string(args);
-			else if (*format == '%')
-				count += print_percent();
-			else
-			{
-				count += write(1, "%", 1);
-				count += write(1, format, 1);
-			}
-		}
-		else
-		{
-			count += write(1, format, 1);
-		}
-		format++;
-	}
-
-	va_end(args);
-
-	return (count);
+if (*format == 'c')
+count += print_char(args);
+else if (*format == 's')
+count += print_string(args);
+else if (*format == '%')
+count += print_percent();
+else
+{
+count += write(1, "%", 1);
+count += write(1, format, 1);
+}
+}
+else
+{
+count += write(1, format, 1);
+}
+format++;
+}
+va_end(args);
+return (count);
 }
