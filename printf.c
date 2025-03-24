74d9bd1c35d047fda37e25e20869505d0eed784d
Hessafa <10685@holbertonstudents.com>
2025-03-24 17:59:39 +0000

    Initial commit

--- printf.c
+++ printf.c
@@ -1,18 +1,63 @@
 #include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_char - Prints a single character.
+ * @args: The argument list containing the character.
+ *
+ * Return: Number of characters printed.
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: The argument list containing the string.
+ *
+ * Return: Number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (!str)
+		str = "(null)";
+
+	while (*str)
+		count += write(1, str++, 1);
+
+	return (count);
+}
+
+/**
+ * print_percent - Prints a literal percent sign.
+ *
+ * Return: Number of characters printed.
+ */
+int print_percent(void)
+{
+	return (write(1, "%", 1));
+}
 
 /**
  * _printf - Produces output according to a format.
- * @format: Format string containing directives.
+ * @format: A character string containing directives.
  *
- * Return: Number of characters printed, or -1 on error.
+ * Return: Number of characters printed (excluding null byte),
+ *         or -1 if an error occurs.
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
 	va_list args;
+	int count = 0;
 
 	if (!format)
-		return (-1); /* Return error for NULL format */
+		return (-1);
 
 	va_start(args, format);
 
@@ -21,26 +66,24 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '\0') /* Handle case where format ends with '%' */
-			{
-				count += _putchar('%');
-				break;
-			}
+			if (!*format)
+				return (-1);
+
 			if (*format == 'c')
-				count += handle_char(args);
+				count += print_char(args);
 			else if (*format == 's')
-				count += handle_string(args);
+				count += print_string(args);
 			else if (*format == '%')
-				count += handle_percent();
+				count += print_percent();
 			else
 			{
-				count += _putchar('%');
-				count += _putchar(*format);
+				count += write(1, "%", 1);
+				count += write(1, format, 1);
 			}
 		}
 		else
 		{
-			count += _putchar(*format);
+			count += write(1, format, 1);
 		}
 		format++;
 	}
