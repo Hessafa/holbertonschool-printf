6c8c28b0a09767c2c1aa97ebe9e11d015e278eba
Hessafa <10685@holbertonstudents.com>
2025-03-28 19:44:21 +0000

    fixing betty

--- print_int.c
+++ print_int.c
@@ -4,16 +4,15 @@
 /**
  * print_number - prints positive long number
  * @n: number to print
- * Return: number of character 
+ * Return: number of characters
  */
-
 int print_number(long n)
 {
 	int count = 0;
 	char digit;
 	int remainder;
 
-	if (n/10)
+	if (n / 10)
 		count += print_number(n / 10);
 
 	remainder = n % 10;
@@ -23,36 +22,30 @@ int print_number(long n)
 	digit = '0' + remainder;
 	count += write(1, &digit, 1);
 
-	return count;
+	return (count);
 }
-
-
-
 
 /**
  * print_int - Print an integer
- * @args: The argument list 
+ * @args: The argument list
  * Return: number of characters to be printed
  */
-
 int print_int(va_list args)
 {
-        int n = va_arg(args, int);
-        long num = n;
+	int n = va_arg(args, int);
+	long num = n;
 	int count = 0;
-        
 
-        if (n == 0)
-                return write(1, "0", 1);
+	if (n == 0)
+		return (write(1, "0", 1));
 
-        if (n < 0)
-        {
-                count += write(1, "-", 1);
-                n = -n;
-        }
+	if (n < 0)
+	{
+		count += write(1, "-", 1);
+		n = -n;
+	}
 
-        count += print_number(num);
+	count += print_number(num);
 
-        return count;
+	return (count);
 }
-
